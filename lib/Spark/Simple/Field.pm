7b605f31275958f9668ef7d486241c97fd96605c
James Laver <jjl@James-Lavers-MacBook-Air.local>
2011-08-16 15:31:40 +0300

    Spark::Simple

---
+++ lib/Spark/Simple/Field.pm
@@ -0,0 +1,9 @@
+package Spark::Simple::Field;
+
+use Moose::Role;
+does 'Spark::Field','Spark::Simple::Node';
+
+1;
+__END__
+
+
---
+++ lib/Spark/Simple/Node.pm
@@ -0,0 +1,30 @@
+package Spark::Simple::Node;
+
+use Moose::Role;
+does 'Spark::Node','Spark::Validates';
+
+use Spark::Types qw(:all);
+
+sub validate {
+    my ($self, $data) = @_;
+    my $result = Spark::Result->new;
+    if ($self->can('_validate')) {
+        my @ret = $self->_validate($data);
+        $result->push(form_result(@ret));
+    }
+    foreach my $f (@{$self->children}) {
+      if (is_SparkValidates($f)) {
+        my $ret = $f->validate($self, $data);
+        $result->push($ret);
+      }
+    }
+    foreach my $v (@{$self->validators}) {
+        my @ret = $v->validate($self, $data);
+        $result->push(form_result(@ret));
+    }
+
+    return $result;
+}
+
+1;
+__END__
