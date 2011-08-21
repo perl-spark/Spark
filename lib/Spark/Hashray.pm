7e0101ccae4ae50c9801c38ecd5ea4e883d65692
Kent Fredric <kentfredric@gmail.com>
2011-08-22 11:03:28 +1200

    Whitespace police Spark::Hashray

--- lib/Spark/Hashray.pm
+++ lib/Spark/Hashray.pm
@@ -6,29 +6,30 @@ use Carp;
 use List::Util 'first';
 
 sub new {
-    my ($package,@items) = @_;
-    croak "New needs an even number of arguments." if (@items % 2);
-    my $ret = bless [],$package;
+    my ($package, @items) = @_;
+    croak('New needs an even number of arguments.') if (@items % 2);
+    my $ret = bless [], $package;
     $ret->push(@items);
-    $ret;
+    return $ret;
 }
 
 sub push {
-    my ($self,@items) = @_; 
-    croak "Push needs an even number of arguments." if (@items % 2);
+    my ($self, @items) = @_;
+    croak('Push needs an even number of arguments.') if (@items % 2);
     while (@items) {
-        push(@$self,[shift @items,shift @items]);
+        push(@$self, [shift @items, shift @items]);
     }
+    return $self;
 }
 
 sub get {
-    my ($self,$key) = @_;
-    (map {$_->[1]} grep {$key eq $_->[0]} @$self);
+    my ($self, $key) = @_;
+    return (map { $_->[1] } grep { $key eq $_->[0] } @$self);
 }
 
 sub get_one {
-    my ($self,$key) = @_;
-    ((first {$key eq $_->[0]} @$self) || [])->[1];
+    my ($self, $key) = @_;
+    return (((first { $key eq $_->[0] } @$self) || [])->[1]);
 }
 
 1;
