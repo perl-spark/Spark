d666b0f562a58c6b7098b8e317f793b30fdd2213
Kent Fredric <kentfredric@gmail.com>
2011-08-22 11:47:05 +1200

    More style changes

--- lib/Spark/Wheel/Textarea/Renderer/HTML.pm
+++ lib/Spark/Wheel/Textarea/Renderer/HTML.pm
@@ -1,13 +1,25 @@
 package Spark::Wheel::Textarea::Renderer::HTML;
 
+use 5.010001;    # // syntax.
+
 use Moose;
 use HTML::Tiny;
+
 with 'Spark::Renderer';
 
 sub render {
-    my ($self,$node,$data) = @_;
-    '<textarea' . (defined $node->name ? ' name="' . $node->name . '"' : '') . '>' . ($data->get_one($node->name)//'') . '</textarea>';
+    my ($self, $node, $data) = @_;
+    my %attributes;
+    $attributes{name} = $node->name if defined $node->name;
+
+    my @attribute_strings = (q{}, map { sprintf q{%s="%s"}, $_, $attributes{$_} } keys %attributes);
+
+    return sprintf '<textarea%s>%s</textarea>',
+      (join q{ }, @attribute_strings),
+      ($data->get_one($node->name) // q{});
 }
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 __END__
