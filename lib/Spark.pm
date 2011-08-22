b0af9403f216ed8ff6fc8137f7bfcabb8e4a8aef
Kent Fredric <kentfredric@gmail.com>
2011-08-22 15:26:29 +1200

    Add Spark.pm to the dist to serve as a placeholder

---
+++ lib/Spark.pm
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+
+package Spark;
+
+# FILENAME: Spark.pm
+# CREATED: 22/08/11 15:22:22 by Kent Fredric (kentnl) <kentfredric@gmail.com>
+# ABSTRACT: A simple yet powerful forms validation system that promotes reuse.
+
+use Moose;
+
+=head1 DESCRIPTION
+
+At this time, most of the documentation for this module is in L<Spark::Form>,
+and this module ( L<Spark> ) is merely a placeholder.
+
+=cut
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+1;
+
