f2f751f9f63645778c60f25d8e396e670beffd6a
Kent Fredric <kentfredric@gmail.com>
2011-08-24 09:24:20 +1200

    Fix issues that make the loader test fail

--- t/lib/Test/Spark/Load/Field/62cdb7020ff920e5aa642c3d4066950dd1f01f4d.pm
+++ t/lib/Test/Spark/Load/Field/62cdb7020ff920e5aa642c3d4066950dd1f01f4d.pm
@@ -1,4 +1,4 @@
-package Spark::Wheel::62cdb7020ff920e5aa642c3d4066950dd1f01f4d;
+package Test::Spark::Load::Field::62cdb7020ff920e5aa642c3d4066950dd1f01f4d;
 
 use Moose;
 extends 'Spark::Wheel::Dummy';
