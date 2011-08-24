2b2037d710fefd40939f61e654aea1a670384ee1
Kent Fredric <kentfredric@gmail.com>
2011-08-24 19:47:34 +1200

    Hack in support for :all because its not there by default

--- lib/Spark/Types.pm
+++ lib/Spark/Types.pm
@@ -2,6 +2,36 @@ package Spark::Types;
 
 use parent 'MooseX::Types::Combine';
 
+use List::Util qw( first );
+
+sub import {
+    my ($class, @types) = @_;
+    my $caller = caller;
+
+    my %types = $class->_provided_types;
+
+    if ( first { $_ eq ':all' } @types ){ 
+      $_->import({ -into => $caller }, q{:all} ) for $class->provide_types_from;
+      return;
+    }
+
+    my %from;
+    for my $type (@types) {
+        unless ($types{$type}) {
+            my @type_libs = $class->provide_types_from;
+
+            die
+                "$caller asked for a type ($type) which is not found in any of the"
+                . " type libraries (@type_libs) combined by $class\n";
+        }
+
+        push @{ $from{ $types{$type} } }, $type;
+    }
+
+    $_->import({ -into => $caller }, @{ $from{ $_ } })
+        for keys %from;
+}
+
 __PACKAGE__->provide_types_from(qw(
   Spark::Types::Classes
   Spark::Types::Roles
