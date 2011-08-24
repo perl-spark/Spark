8134af022de85eb9f83fdea210697e9d18aded00
Kent Fredric <kentfredric@gmail.com>
2011-08-24 17:37:43 +1200

    minor weaver enhancement, doc attributes in Spark::Container

--- lib/Spark/Container.pm
+++ lib/Spark/Container.pm
@@ -3,6 +3,12 @@ package Spark::Container;
 use Moose::Role;
 use Spark::Types qw(Hashray);
 
+=attr children
+
+  my $hash = $object->children();
+
+=cut
+
 has children => (
     isa     => Hashray,
     is      => 'ro',
